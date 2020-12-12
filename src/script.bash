8bd53fd656101f2aa2fc410f48dad603c939773e
pintob <pintob_info@yahoo.fr>
2020-12-12 15:29:21 +0100

    bash fix

--- src/script.bash
+++ src/script.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 POLI=1
-AUTH=1
+AUTH=2
 
 
 gnome-terminal --title="Server"  -- python3 server.py 
 
-for i in $(seq 1 $END); do 
+for i in $(seq 1 $POLI); do 
 	gnome-terminal --title="Politician $i" -- python3 politician.py;
 done
 
-for i in $(seq 1 $END); do 
+for i in $(seq 1 $AUTH); do 
 	gnome-terminal --title="Author $i"  -- python3 author.py
 done
